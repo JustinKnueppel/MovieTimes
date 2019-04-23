41d25b6e2d158b199fbbfde743ff507c9a62901b
Justin <justinknueppel@gmail.com>
2019-04-23 10:58:28 -0400

    Working template with axios

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,50 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
 
-const date = '2019-04-17';
-const url = `https://www.amctheatres.com/movie-theatres/showtimes/all/${date}/amc-lennox-town-center-24/all`;
+const baseURL = 'https://www.amctheatres.com';
 
-axios.get(url)
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((err) => {
-        console.log(err);
+formatDate = function(date) {
+    dd = date.getDate();
+    dd = dd >= 10 ? dd : `0${dd}`
+    mm = date.getMonth() + 1;
+    mm = mm >= 10 ? mm : `0${mm}`
+    yyyy = date.getFullYear();
+    return `${yyyy}-${mm}-${dd}`
+    // "X-AMC-Vendor-Key": info['api-key'],
+}
+
+
+const date = formatDate(new Date());
+let theatres = ['amc-lennox-town-center-24', 'amc-dublin-village-18']
+let url = `${baseURL}/movie-theatres/showtimes/all/${date}/${theatres[1]}/all`;
+
+let movies = [];
+
+theatres = axios(url);
+theatres.then((response) => {
+    const $ = cheerio.load(response.data);
+    $('div[class="ShowtimesByTheatre-maincol-scroll"]').find('div[class="ShowtimesByTheatre-film"]').each((i, e) => {
+        movies.push($(e));
     });
+    let movieTitle = $(movies[0]).find('a.MovieTitleHeader-title > h2').text();
+    console.log(movieTitle);
+    let movieLink = `${baseURL}${$(movies[0]).find('a.MovieTitleHeader-title').attr('href')}`;
+    console.log(movieLink);
+    let movieTimes = [];
+    $(movies[0]).find('div.Showtime a.Btn').each((i, e) => {
+        movieTimes.push($(e).text());
+    });
+    console.log(movieTimes);
+    let movieTimeLinks = []
+    $(movies[0]).find('div.Showtime a.Btn').each((i, e) => {
+        movieTimeLinks.push($(e).attr('href'));
+    });
+    console.log(movieTimeLinks);
+
+})
+.catch((err) => {
+    console.log("Error");
+});
+
+
+

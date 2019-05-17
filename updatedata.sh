abdcbd4588d979940505529ec616e9aeeae0a3a7
Justin <justinknueppel@gmail.com>
2019-05-17 07:46:24 -0400

    Update data for 2019-05-17

--- updatedata.sh
+++ updatedata.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+cd $DIR
 node Backend/app/updateDB.js
 node tempupdate.js
 tsc --project tsconfig_temp.json

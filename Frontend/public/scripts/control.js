148b6df6ba9e659a8f25695a1f8ac8531e251608
Justin Knueppel <justinknueppel@gmail.com>
2019-06-13 20:44:32 -0400

    Switch to javascript to get firebase to work

---
+++ Frontend/public/scripts/control.js
@@ -0,0 +1,83 @@
+function getCheckedTheatres() {
+    let checkedTheatres = [];
+
+    for (let theatre of AMCtheatres) {
+        if (
+            
+                document.querySelector(
+                    `#theatre-options input[name="${theatre.name}"]`
+                )
+            .checked
+        ) {
+            checkedTheatres.push(theatre.id);
+        }
+    }
+
+    return checkedTheatres;
+}
+
+/**
+ * Return a date object representing the time limit inside the given element.
+ * @param timeElem div containing the time selectors.
+ */
+function parseTimeInput(timeElem) {
+    let hourDD = timeElem.querySelector(
+        'select[name="hours"]'
+    );
+    let hour = parseInt(hourDD.options[hourDD.selectedIndex].value);
+
+    let ampmDD = timeElem.querySelector(
+        'select[name="ampm"]'
+    );
+    let ampm = ampmDD.options[ampmDD.selectedIndex].value;
+
+    if (ampm === 'pm') {
+        hour += 12;
+    }
+
+    let minuteDD = timeElem.querySelector(
+        'select[name="minutes"]'
+    );
+    let minute = parseInt(minuteDD.options[minuteDD.selectedIndex].value);
+
+    let time = new Date();
+    time.setHours(hour);
+    time.setMinutes(minute);
+    time.setSeconds(0);
+
+    return time;
+}
+
+/**
+ * Get the time options that were filled in.
+ */
+function getTimeConstraints() {
+    let timeStartElem = document.getElementById('time-start');
+    let timeEndElem = document.getElementById('time-end');
+
+    let startTime = parseTimeInput(timeStartElem);
+    let endTime = parseTimeInput(timeEndElem);
+
+    return { start: startTime, end: endTime };
+}
+
+// Control elements of page
+/**
+ * Document already loaded from loaddata script.
+ */
+document.addEventListener('click', function(event) {
+    if (!(event.target instanceof Element)) {
+        console.log('Event not a target of element');
+        return;
+    }
+
+    /*
+     * Deal with filtering.
+     */
+    if (event.target.matches('#submit-filter')) {
+        let checkedTheatres = getCheckedTheatres();
+        let times = getTimeConstraints();
+        let data = filterData(checkedTheatres, times.start, times.end);
+        loadData(data);
+    }
+});
---
+++ Frontend/public/scripts/loaddata.js
@@ -0,0 +1,311 @@
+
+let AMCtheatres= [
+    { id: 'amc-lennox-town-center-24', name: 'AMC Lennox', link: 'https://www.amctheatres.com/movie-theatres/amc-lennox-town-center-24' },
+    { id: 'amc-dublin-village-18', name: 'AMC Dublin Village', link: 'https://www.amctheatres.com/movie-theatres/amc-dublin-village-18' },
+    { id: 'amc-columbus-10', name: 'AMC Hilliard', link: 'https://www.amctheatres.com/movie-theatres/amc-columbus-10' }
+];
+
+/**
+ * Format a date object into a yyyy-mm-dd date string.
+ * @param date Date to be formatted.
+ */
+function formatDate(date) {
+    let dd = date.getDate();
+    dd = dd >= 10 ? dd : `0${dd}`;
+    let mm = date.getMonth() + 1;
+    mm = mm >= 10 ? mm : `0${mm}`;
+    let yyyy = date.getFullYear();
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+/**
+ * Display theatre options to the user.
+ */
+function loadTheatreOptions() {
+    let options = document.querySelector('#theatre-options ul');
+
+    for (let theatre of AMCtheatres) {
+        let li = document.createElement('li');
+
+        let node = document.createElement('input');
+        node.setAttribute('type', 'checkbox');
+        node.setAttribute('value', theatre['id']);
+        node.setAttribute('name', theatre['name']);
+        node.checked = true;
+
+        li.appendChild(node);
+
+        li.append(theatre['name']);
+
+        options.appendChild(li);
+    }
+}
+
+/**
+ * Return the current hour in 12 hour format along with an am/pm indicator.
+ */
+function getCurrentHour() {
+    let date = new Date();
+    let hour = date.getHours();
+
+    let ampm = hour <= 11 ? 'am' : 'pm';
+
+    if (hour > 12) {
+        hour -= 12;
+    }
+
+    return [hour, ampm];
+}
+
+/**
+ * Display time filtering options.
+ */
+function loadTimeOptions() {
+    let [currentHour, ampm] = getCurrentHour();
+
+    // Generate values for the start times.
+    let startTime = document.getElementById('time-start');
+
+    let startHours = startTime.querySelector('select[name="hours"]');
+
+    for (let i = 1; i <= 12; i++) {
+        let hour = document.createElement('option');
+        hour.setAttribute('value', i.toString());
+        if (i === currentHour) {
+            hour.setAttribute('selected', 'selected');
+        }
+        hour.innerText = i.toString();
+        startHours.appendChild(hour);
+    }
+
+    let startMinutes = startTime.querySelector(
+        'select[name="minutes"]'
+    );
+
+    for (let i of ['00', '15', '30', '45', '59']) {
+        let minute = document.createElement('option');
+        minute.setAttribute('value', i);
+        minute.innerText = i;
+        startMinutes.appendChild(minute);
+    }
+
+    startTime
+        .querySelector(
+            `select[name="ampm"] option[value="${ampm}"]`
+        )
+        .setAttribute('selected', 'selected');
+
+    // Generate values for the end times.
+    let endTime = document.getElementById('time-end');
+
+    let endHours = endTime.querySelector('select[name="hours"]');
+
+    for (let i = 1; i <= 12; i++) {
+        let hour = document.createElement('option');
+        hour.setAttribute('value', i.toString());
+        if (i === 11) {
+            hour.setAttribute('selected', 'selected');
+        }
+        hour.innerText = i.toString();
+        endHours.appendChild(hour);
+    }
+
+    let endMinutes = endTime.querySelector('select[name="minutes"]');
+
+    for (let i of ['00', '15', '30', '45', '59']) {
+        let minute = document.createElement('option');
+        minute.setAttribute('value', i);
+        if (i === '59') {
+            minute.setAttribute('selected', 'selected');
+        }
+        minute.innerText = i;
+        endMinutes.appendChild(minute);
+    }
+}
+
+/**
+ * Return the theatre info for the given theatre and date.
+ * @param date Date of movie info to get.
+ * @param theatre Unique name for theatre.
+ */
+function getTheatreInfo(date, theatre) {
+    //TODO: Access backend to get real data
+    return theatreinfo[theatre];
+}
+/**
+ * Retrieve the stored JSON for all theatres for today.
+ */
+function getData() {
+    let data = {};
+    for (let theatre of AMCtheatres) {
+        data[theatre.id] = getTheatreInfo(new Date(), theatre.id);
+    }
+
+    return data;
+}
+
+/**
+ * Convert a time string into a date object.
+ * @param time hh:mm[am/pm] time string.
+ */
+function timeToDate(time) {
+    let regex = /(\d{1,2}):(\d{1,2})(am|pm)/;
+    let match = regex.exec(time);
+
+    let hour = parseInt(match[1]);
+    if (match[3] === 'pm' && hour !== 12) {
+        hour += 12;
+    }
+    if (match[3] === 'am' && hour === 12) {
+        hour = 0;
+    }
+    let minute = parseInt(match[2]);
+
+    let date = new Date();
+    date.setHours(hour);
+    date.setMinutes(minute);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    return date;
+}
+
+/**
+ * Convert theatre information into a list of showtimes.
+ * @param theatre Unique theatre name.
+ * @param theatreInfo Information about movie showtimes at a given theatre.
+ */
+function getShowtimes(theatre, theatreInfo) {
+    let showtimes = [];
+
+    for (let movie of theatreInfo) {
+        for (let showtime of movie.times) {
+            showtimes.push({
+                title: movie.title,
+                movieLink: movie.link,
+                showtime: showtime.time,
+                showtimeLink: showtime.link,
+                theatre: theatre.name,
+                theatreLink: theatre.link,
+                sortTime: timeToDate(showtime.time).getTime()
+            });
+        }
+    }
+
+    return showtimes;
+}
+
+/**
+ * Load a row into the movie table representing the showtime.
+ * @param showtime Showtime information.
+ */
+function loadShowtime(showtime) {
+    let tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    let time = document.createElement('a');
+    time.setAttribute('href', showtime.showtimeLink);
+    time.innerText = showtime.showtime;
+    td.appendChild(time);
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    let movie = document.createElement('a');
+    movie.setAttribute('href', showtime.movieLink);
+    movie.innerText = showtime.title;
+    td.appendChild(movie);
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    let theatre = document.createElement('a');
+    theatre.setAttribute('href', showtime.theatreLink);
+    theatre.innerText = showtime.theatre;
+    td.appendChild(theatre);
+    tr.appendChild(td);
+
+    return tr;
+}
+
+/**
+ * Add showtimes given to the movie table.
+ * @param showtimes List of showtimes to be added to table.
+ */
+function loadShowtimes(showtimes) {
+    let movieTable = document.getElementById('movie-table');
+
+    for (let showtime of showtimes) {
+        movieTable.appendChild(loadShowtime(showtime));
+    }
+}
+
+/*
+ * Load all data first, filter later.
+ */
+const ALL_DATA = getData();
+
+/*
+ * Filter all data by given constraints.
+ */
+function filterData(
+    theatreIDs,
+    startTime,
+    endTime
+) {
+    let filterData = {};
+
+    for (let theatre of Object.keys(ALL_DATA)) {
+        // Filter for theatre
+        if (theatreIDs.indexOf(theatre) >= 0) {
+            filterData[theatre] = JSON.parse(JSON.stringify(ALL_DATA[theatre]));
+            // Filter for time
+            for (let movie of filterData[theatre]) {
+                movie.times = movie.times.filter(timeObj => {
+                    let time = timeToDate(timeObj.time);
+                    return time >= startTime && time <= endTime;
+                });
+            }
+        }
+    }
+
+    return filterData;
+}
+/**
+ * Load in showtimes in chronological order.
+ */
+function loadData(theatresData) {
+    // Remove old data
+    let table = document.getElementById('movie-table');
+
+    for (let tr of document.querySelectorAll(
+        '#movie-table > tr:not([table-headers])'
+    )) {
+        table.removeChild(tr);
+    }
+
+    // Load new data
+    let showtimes = [];
+
+    for (let theatre of AMCtheatres) {
+        if (theatresData[theatre.id]) {
+            showtimes.push(
+                ...getShowtimes(theatre, theatresData[theatre.id])
+            );
+        }
+    }
+
+    loadShowtimes(
+        showtimes.sort((s1, s2) => {
+            return s1.sortTime - s2.sortTime;
+        })
+    );
+}
+
+/**
+ * Once DOM has loaded, load in data.
+ */
+window.onload = () => {
+    // Populate filter options.
+    loadTheatreOptions();
+    loadTimeOptions();
+
+    loadData(ALL_DATA);
+};
